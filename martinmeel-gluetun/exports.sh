74b29646d5f7c49e9603843f6cae508428171e93
MartinMeel <meelmartin@hotmail.com>
2025-12-14 11:52:15 +0100

    1

--- martinmeel-gluetun/exports.sh
+++ martinmeel-gluetun/exports.sh
@@ -1,3 +1,3 @@
 export WINDSCRIBE_PRIVATE_KEY="exact-Windscribe-username"
 export WINDSCRIBE_PRESHARED_KEY="exact-Windscribe-password"
-export WIREGUARD_ADDRESSES="ip-address"
+export WINDSCRIBE_ADDRESSES="ip-address"
