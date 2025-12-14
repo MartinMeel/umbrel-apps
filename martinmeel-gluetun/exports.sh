6e1d56ca12ec3fe75b1b3a556dc72164903deedb
MartinMeel <meelmartin@hotmail.com>
2025-12-14 08:34:12 +0100

    1

--- martinmeel-gluetun/exports.sh
+++ martinmeel-gluetun/exports.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Export Gluetun container name for other apps to reference
-export APP_GLUETUN_CONTAINER_NAME="gluetun_server_1"
+#export APP_GLUETUN_CONTAINER_NAME="gluetun_server_1"
 
 # Export VPN status endpoint
-export APP_GLUETUN_API_URL="http://gluetun_server_1:8688"
+#export APP_GLUETUN_API_URL="http://gluetun_server_1:8688"
 
 # Network mode for other containers to use
-export APP_GLUETUN_NETWORK_MODE="container:gluetun_server_1"
+#export APP_GLUETUN_NETWORK_MODE="container:gluetun_server_1"
 
 export WINDSCRIBE_USERNAME="efdqk1p2-97zvbkq"
 export WINDSCRIBE_PASSWORD="d2fw9tengj"
