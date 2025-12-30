7f24e7a888610227532dd4c5b0b23b5581c293d2
MartinMeel <meelmartin@hotmail.com>
2025-12-30 12:21:43 +0100

    e

--- martinmeel-spotweb/exports.sh
+++ martinmeel-spotweb/exports.sh
@@ -1,2 +1,4 @@
 export SPOTWEB_DB_PASSWORD=$(derive_entropy "spotweb-db-password" | head -c32)
+echo "DB Password: $SPOTWEB_DB_PASSWORD"
 export MYSQL_ROOT_PASSWORD=$(derive_entropy "spotweb-mysql-root" | head -c32)
+echo "Root Password: $MYSQL_ROOT_PASSWORD"
