0c7db1ce43056ae7346345d761b3b94a5cf5278e
MartinMeel <meelmartin@hotmail.com>
2026-05-14 17:18:56 +0200

    Remove sabnzbd paths from ALLOWED_PATHS
    Removed sabnzbd download paths from allowed paths.

--- martinmeel-compose-path-editor/server_runtime.py
+++ martinmeel-compose-path-editor/server_runtime.py
@@ -11,8 +11,6 @@ PORT = int(os.environ.get('PORT','8099'))
 ALLOWED_PATHS = [
   '${UMBREL_ROOT}/home/Downloads/qbittorrent/complete',
   '${UMBREL_ROOT}/home/Downloads/qbittorrent/incomplete',
-  '${UMBREL_ROOT}/home/Downloads/sabnzbd/complete',
-  '${UMBREL_ROOT}/home/Downloads/sabnzbd/incomplete',
   '${UMBREL_ROOT}/home/Downloads/Films',
   '${UMBREL_ROOT}/home/Downloads/Films2',
   '${UMBREL_ROOT}/home/Downloads/TVSerie',
