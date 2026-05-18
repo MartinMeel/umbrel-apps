b2606aa1f71adea0bfcc492c18b299536cb0e618
MartinMeel <meelmartin@hotmail.com>
2026-05-18 11:34:34 +0200

    Add files via upload

--- martinmeel-filebrowser-quantum/exports.sh
+++ martinmeel-filebrowser-quantum/exports.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-export APP_FILEBROWSER_QUANTUM_HOST="filebrowser-quantum_server_1"
-export APP_FILEBROWSER_QUANTUM_PORT="80"
+export APP_FILEBROWSER_QUANTUM_HOST="martinmeel-filebrowser-quantum_server_1"
+export APP_FILEBROWSER_QUANTUM_PORT="8080"
+export APP_MARTINMEEL_FILEBROWSER_QUANTUM_HOST="martinmeel-filebrowser-quantum_server_1"
+export APP_MARTINMEEL_FILEBROWSER_QUANTUM_PORT="8080"
